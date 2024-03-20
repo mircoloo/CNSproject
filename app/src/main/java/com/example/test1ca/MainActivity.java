23cefe0f2bed432b6b701eeb6017be3e3579b70f
davidespada99 <spadads@gmail.com>
2024-03-20 12:22:48 +0100

    changed button09 position

--- app/src/main/java/com/example/test1ca/MainActivity.java
+++ app/src/main/java/com/example/test1ca/MainActivity.java
@@ -45,7 +45,7 @@ public class MainActivity extends AppCompatActivity {
             return;
         }
 
-        int[] numbers = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
 
         // Convert int array to Integer array
         Integer[] integerNumbers = Arrays.stream(numbers).boxed().toArray(Integer[]::new);
