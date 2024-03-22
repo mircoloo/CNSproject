cd2a2c7fe757a9c1a923143f9ece9420b1666c01
davidespada99 <spadads@gmail.com>
2024-03-22 17:39:24 +0100

    added csv log

--- app/src/main/java/com/example/test1ca/MainActivity.java
+++ app/src/main/java/com/example/test1ca/MainActivity.java
@@ -6,9 +6,14 @@ import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.widget.Button;q
 import android.widget.EditText;
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -18,20 +23,42 @@ public class MainActivity extends AppCompatActivity {
 
     static String PIN = "1234";
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initializeButtons();
 
-
     }
 
+    public void csvWriter( String content ) throws IOException {
+        File file = new File( getFilesDir(), "data.csv");
+        FileWriter fw = new FileWriter(file.getAbsoluteFile(), true);
+        BufferedWriter bw = new BufferedWriter(fw);
+
+        if (!file.exists()) {
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        if(file.length()<=0){
+            bw.write("X,Y, GTM, GTm, EA, AA, bID\n");
+        }
+
+        bw.write(content);
+        bw.close();
+    }
 
     public void initializeButtons() {
         String buttonTag = "button";
         ViewGroup rootLayout = findViewById(android.R.id.content);
         List<View> buttons = findViewsByTag(rootLayout, buttonTag);
+
 
         if (buttons.size() != 10) {
             Log.e("MainActivity", "Expected 10 buttons with tag 'myButton', found: " + buttons.size());
@@ -40,11 +67,18 @@ public class MainActivity extends AppCompatActivity {
 
         int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
 
-        // Convert int array to Integer array
+//        Convert int array to Integer array
         Integer[] integerNumbers = Arrays.stream(numbers).boxed().toArray(Integer[]::new);
 
-        // Shuffle the array
+//        Shuffle the array
         Collections.shuffle(Arrays.asList(integerNumbers));
+
+//        setId() for each button in ascending order
+        int id=0;
+        for (View button : buttons) {
+            button.setId(id);
+            id++;
+        }
 
         int index = 0;
         for (View buttonView : buttons) {
@@ -71,10 +105,22 @@ public class MainActivity extends AppCompatActivity {
         return views;
     }
 
-    public void onClicked(View v){
+    public void onClicked(View v) throws IOException {
         View displayTextView = findViewById(R.id.display_text);
         EditText displayText = (EditText) displayTextView;
+
+        View displayIdsView = findViewById(R.id.display_ids);
+        EditText displayIds = (EditText) displayIdsView;
+        String idsDisplay = String.valueOf(displayIds.getText());
+
+
         Button b = (Button) v;
+        Log.d("button_ID", String.valueOf(b.getId()));
+        csvWriter(String.valueOf(b.getId())+ "\n");
+
+        idsDisplay += String.valueOf(b.getId());
+        displayIds.setText(idsDisplay);
+
         String textToWrite = String.valueOf(displayText.getText()) + String.valueOf(b.getText());
 
         if(textToWrite.length() >= PIN.length()){
@@ -84,13 +130,14 @@ public class MainActivity extends AppCompatActivity {
             else{
                 Log.d("PINRESULT", "Wrong PIN");
             }
-
-
             textToWrite = "";
             initializeButtons();
         }
 
         displayText.setText(textToWrite);
+
+
+
     }
 
     @Override
@@ -100,7 +147,7 @@ public class MainActivity extends AppCompatActivity {
         int screenWidth = displayMetrics.widthPixels; // Larghezza totale dello schermo in pixel
         int screenHeight = displayMetrics.heightPixels; // Altezza totale dello schermo in pixel
         String toDisplay = String.valueOf(displayData.getText());
-
+        String toWrite = "";
         // Calcola il numero di pixel toccati utilizzando la grandezza totale dello schermo
         int totalPixels = screenWidth * screenHeight;
         float touchedAreaSize = event.getSize() * totalPixels;
@@ -110,14 +157,23 @@ public class MainActivity extends AppCompatActivity {
         float touchMinor = event.getTouchMinor();
         float touchedArea = (float) (Math.PI * (touchMajor / 2) * (touchMinor / 2));
 
+
         if (event.getAction() == MotionEvent.ACTION_DOWN) {
             toDisplay =   "X:" + event.getX() + "\n Y:" + event.getY() + "\n" + " GTM: " + touchMajor + ", GTm: " + touchMinor +
                     "\nEllipse Area : " + touchedAreaSize + "\n Actual Area : " + touchedArea;
             Log.d("TouchEvent", event.getPressure() +  " (" + event.getX() + "," + event.getY() + ")");
             Log.d("TouchEvent",  "GetSize() --- Size (%): " + event.getSize() + ", Size (px): " + touchedAreaSize);
             Log.d("TouchEvent",  "GetTouchMajor() GetTouchMinor() --- GetTouchMajor: " + touchMajor + ", GetTouchMinor: " + touchMinor + ", Size (px): " + touchedArea);
-
+            toWrite = event.getX() + "," + event.getY() + "," + touchMajor + "," + touchMinor +
+                    "," + touchedAreaSize + "," + touchedArea + ",";
+            try {
+                csvWriter(toWrite);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
         }
+
+
 
         displayData.setText(toDisplay);
 
